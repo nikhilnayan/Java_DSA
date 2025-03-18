77a3f213669902721b91618aedaa3f64d37debc4
Nikhil Nayan <nayannikhil25@gmail.com>
2025-03-18 14:19:38 +0530

    sumOfSortedArray

--- sumOfSortedArray.java
+++ sumOfSortedArray.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
-public class Strings {
+public class sumOfSortedArray {
     
-
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
+        
     }
 }
